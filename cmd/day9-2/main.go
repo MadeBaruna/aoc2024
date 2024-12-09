8418920abfd50c2f9f3d69c65fdc7b59ae6c09d5
Made Baruna <made.setia@gmail.com>
2024-12-10 00:19:39 +0800

    day 8 part 2

--- cmd/day9-2/main.go
+++ cmd/day9-2/main.go
@@ -2,112 +2,116 @@ package main
 
 import (
 	"fmt"
+	"slices"
 	"strconv"
 	"strings"
 
 	"baruna.me/aoc2024/internal/lib"
 )
 
+type Block struct {
+	id     int
+	length int
+	moved  bool
+}
+
 func main() {
-	content, err := lib.ReadFile(true)
+	content, err := lib.ReadFile(false)
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Println(content)
+	// fmt.Println(content)
 
 	chars := strings.Split(strings.TrimSpace(content), "")
-	blocks := make([]int, 0)
+	blocks := make([]*Block, 0)
 
 	id := -1
 	block := true
 	for _, char := range chars {
 		n, _ := strconv.Atoi(char)
 
-		numbers := make([]int, n)
 		num := -1
 		if block {
 			id++
 			num = id
 		}
-		for i := 0; i < n; i++ {
-			numbers[i] = num
-		}
-		blocks = append(blocks, numbers...)
+		blocks = append(blocks, &Block{num, n, false})
 
 		block = !block
 	}
 
-	fmt.Println(blocks)
-	fill(blocks)
+	printBlocks(blocks)
+	fmt.Println()
+	move(blocks)
 }
 
-func fill(blocks []int) {
-	l := len(blocks)
+func move(blocks []*Block) {
+	_blocks := make([]*Block, len(blocks))
+	copy(_blocks, blocks)
 
-	length := 0
-	cur := blocks[l-1]
-	min := int(^uint(0) >> 1)
-	for i := l - 1; i >= 0; i-- {
-		if blocks[i] == cur {
-			length++
+	for i := len(_blocks) - 1; i >= 0; i-- {
+		_b := _blocks[i]
+
+		if _b.id == -1 || _b.moved {
 			continue
 		}
 
-		if cur == -1 {
-			cur = blocks[i]
-			length = 1
+		s := findSpace(blocks, _b)
+		// fmt.Println(i, "-> space index", s, "for", _b.id)
+		if s == -1 {
 			continue
 		}
 
-		if cur < min {
-			s := findSpace(blocks, length, i)
-			fmt.Println(cur, length, s)
-			if s > -1 {
-				for j := s; j < s+length; j++ {
-					blocks[j] = cur
-				}
-				for j := i + 1; j <= i+length; j++ {
-					blocks[j] = -1
-				}
-			}
-			fmt.Println(blocks)
-			min = cur
-		}
+		blocks[s].length -= _b.length
+		bCopy := *_b
 
-		cur = blocks[i]
-		length = 1
+		_b.moved = true
+		blocks = slices.Insert(blocks, s, &bCopy)
+		_b.id = -1
+		// printBlocks(blocks)
 	}
 
-	fmt.Println(blocks)
+	printBlocks(blocks)
+
 	total := 0
-	for i, n := range blocks {
-		if n == -1 {
-			continue
+	i := 0
+	for _, b := range blocks {
+		for j := 0; j < b.length; j++ {
+			if b.id > -1 {
+				total += i * b.id
+			}
+			i++
 		}
-		total += i * n
 	}
+	fmt.Println()
 	fmt.Println(total)
 }
 
-func findSpace(blocks []int, length int, limit int) int {
-	space := 0
-	start := 0
-	cur := blocks[0]
-	for i := 0; i < limit; i++ {
-		if blocks[i] != cur {
-			cur = blocks[i]
-			start = i
-			space = 0
+func findSpace(blocks []*Block, b *Block) int {
+	for i := range blocks {
+		block := blocks[i]
+		if block.id == b.id {
+			return -1
 		}
-
-		if blocks[i] == -1 {
-			space++
-			if space == length {
-				return start
-			}
+		if block.id != -1 {
+			continue
+		}
+		if block.length >= b.length {
+			return i
 		}
 	}
 
 	return -1
+}
+
+func printBlocks(blocks []*Block) {
+	for _, b := range blocks {
+		if b.id == -1 {
+			fmt.Printf("[. %d]", b.length)
+		} else {
+			fmt.Printf("[%d %d]", b.id, b.length)
+		}
+	}
+	fmt.Println()
 }
